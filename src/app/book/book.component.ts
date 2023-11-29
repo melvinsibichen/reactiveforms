7a3dede813b5724493d27012b60787a409fa9ab3
Melvin Sibichen <melvinsibichen1020@gmail.com>
2023-11-29 13:16:58 +0530

    bug fix

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,4 +1,4 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -6,30 +6,30 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './book.component.html',
   styleUrls: ['./book.component.scss']
 })
-export class BookComponent implements OnInit {
-
-  bookForm!: FormGroup;
-
+export class BookComponent {
   constructor(private fb: FormBuilder) { }
+  submit = false
 
-  ngOnInit(): void {
-    this.bookForm = this.fb.group({
-      bookName: ['', Validators.required],
-      author: ['', Validators.required],
-      publication: ['', Validators.required],
-      price: [null, [Validators.required, Validators.min(0)]],
-      genre: ['', Validators.required],
-      publishedDate: [null, Validators.required],
-      country: ['', Validators.required],
-      description: ['', Validators.required],
-    });
+  bookForm = this.fb.group({
+
+    bookName: ['', Validators.required],
+    author: ['', Validators.required],
+    publication: ['', Validators.required],
+    price: [null, [Validators.required, Validators.min(0)]],
+    genre: ['', Validators.required],
+    publishedDate: [null, Validators.required],
+    country: ['', Validators.required],
+    description: ['', Validators.required],
+  })
+
+  get f() {
+    return this.bookForm.controls;
   }
 
   onSubmit() {
-    if (this.bookForm.valid) {
-      // Process the form data (e.g., send it to a server)
-      console.log(this.bookForm.value);
-    }
+    this.submit = true
+    console.log('clicked');
+    console.log('f', this.f);
   }
 
 }
